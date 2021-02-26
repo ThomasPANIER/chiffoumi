8eeeea7386ec700e3571833108e230ed5116b2d0
ThomasPANIER <thomaspanier.pro@gmail.com>
2021-02-26 11:46:23 +0100

    show welcome

---
+++ js/chiffoumi.js
@@ -0,0 +1,10 @@
+//Code js pour le lancement d'un jeu du style chiffoumi.
+
+//pop-up d'affichage "welcome"
+alert("Welcome !");
+
+//pop-up fenetre pour entrer le nom, puis pop-up merci + nom entré
+var nom = prompt("Entrez votre nom: ");
+    //var name = prompt();
+
+alert("Merci " + nom + "!");
