d6ff55bbe7e18ebd1a0f651649c3cccdfa1c4303
ThomasPANIER <thomaspanier.pro@gmail.com>
2021-02-26 19:56:13 +0100

    3 pop-up finish

--- js/chiffoumi.js
+++ js/chiffoumi.js
@@ -1,7 +1,7 @@
 //Code js pour le lancement d'un jeu du style chiffoumi.
 
 //pop-up d'affichage "welcome"
-alert("Welcome !");
+alert("Bonjour !");
 
 //pop-up fenetre pour entrer le nom, puis pop-up merci + nom entré
 var nom = prompt("Entrez votre nom: ");
